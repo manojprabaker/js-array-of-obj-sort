f5c18aa3a3545e0ddc2b02f20e154d5f9abee57d
manojprabaker <manojprabaker999@gmail.com>
2023-03-24 12:28:26 +0530

    add

---
+++ main.js
@@ -0,0 +1,47 @@
+
+ 
+ 
+ //11. Sort an array of JavaScript objects based on age
+
+let employees = [
+
+{
+
+firstName: 'John',
+
+lastName: 'Doe',
+
+age: 27,
+
+joinedDate: 'December 15, 2017'
+
+},
+
+{
+
+firstName: 'Ana',
+
+lastName: 'Rosy',
+
+age: 25,
+
+joinedDate: 'January 15, 2019'
+
+},
+
+{
+
+firstName: 'Zion',
+
+lastName: 'Albert',
+
+age: 30,
+
+joinedDate: 'February 15, 2011'
+
+}
+
+];
+let sorted=employees.sort((a1,b1)=> b1.age-a1.age);
+console.log(sorted);
+
